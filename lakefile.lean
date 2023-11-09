cc4b3be1c11979762a8eff26299641ceec9e6a0a
Mayant Mukul <mayantmukul@outlook.in>
2023-11-08 21:58:23 -0800

    add second proofs lib

--- lakefile.lean
+++ lakefile.lean
@@ -1,14 +1,18 @@
 import Lake
 open Lake DSL
 
-package «core» {
+package «proofs» {
   -- add any package configuration options here
 }
 
 require mathlib from git
   "https://github.com/leanprover-community/mathlib4.git"
 
-@[default_target]
 lean_lib «Core» {
+  -- add any library configuration options here
+}
+
+@[default_target]
+lean_lib «Proofs» {
   -- add any library configuration options here
 }
